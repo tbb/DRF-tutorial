36859e0d698da83280b5e83343fca3f63791247e
tbb <dmitry.tbb@yandex.ru>
2018-06-24 16:20:45 +0300

    Stage 5. Use hyper linked serializers. Add highlighted view.

--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,23 @@
 from django.contrib.auth.models import User
+
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+    })
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -20,6 +34,15 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly)
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer, )
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class UserList(generics.ListAPIView):
