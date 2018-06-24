ebb0e3b2da41738d82cddf320249a9d4a45ef582
tbb <dmitry.tbb@yandex.ru>
2018-06-24 17:58:52 +0300

    Stage 6. View Sets and Routers.

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
+router = DefaultRouter()
+router.register('snippets', views.SnippetViewSet)
+router.register('users', views.UserViewSet)
 
-urlpatterns = format_suffix_patterns(urlpatterns = [
-    path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-])
-
-urlpatterns += [
+urlpatterns = [
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -3,21 +3,13 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import renderers
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format),
-    })
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -45,11 +37,30 @@ class SnippetHighlight(generics.GenericAPIView):
         return Response(snippet.highlighted)
 
 
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    Этот набор представлений автоматически создаёт действия 'list', 'create', 'retrieve',
+    'update' and 'destroy'.
+
+    Дополнительно мы добавим действие 'highlight'.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly, )
+
+    @action(renderer_classes=[renderers.StaticHTMLRenderer], detail=True)
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Этот набор представлений автоматически создаёт действия 'list' и 'detail'.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
